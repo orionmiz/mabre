e1b3bc76451f1d400a3f0a4ea0e1b1b32e4b1278
orionmiz <x3ph3r@naver.com>
2022-07-04 03:09:18 +0900

    Check an error message from token fetching

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -24,7 +24,7 @@ export const getAccessFromCode = async (
   });
 
   if (res.status !== 200) {
-    throw new Error("Failed to get token");
+    throw new Error(JSON.stringify(await res.json()));
   }
 
   const info = await res.json();
