04d924d7486a5d176cb9a1a890fa65fdea196a1e
orionmiz <x3ph3r@naver.com>
2022-07-12 11:39:56 +0900

    Configure host_hub

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,4 +12,5 @@ export const CDN = process.env.CDN as string;
 export const host = isSecureMode
   ? (process.env.NEXT_PUBLIC_HOST as string)
   : "http://localhost:3000";
-export const host_game = process.env.GAME_HOST as string;
+export const host_game = process.env.NEXT_PUBLIC_GAME_HOST as string;
+export const host_hub = `http://${host_game}:7070`;
